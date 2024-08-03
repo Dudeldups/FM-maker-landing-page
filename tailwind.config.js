04323032e0d35fe0ef4eb8799bb4fdd37451ad69
Dudeldups <130840918+Dudeldups@users.noreply.github.com>
2024-08-03 21:25:08 +0200

    updated tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,13 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      backgroundImage: {
+        "hero-pattern": "url('/assets/images/bg-hero-squiggle.svg')",
+        "footer-pattern": "url('/assets/images/bg-footer-squiggle.svg')",
+      },
+
       colors: {
-        green: "#3EE9E5",
+        cyan: "#3EE9E5",
         blue: "#093F68",
         darkblue: "#080C20",
         grey: "#777F98",
