829da82430a6533c0a49843cd7a7414b3bb7a2d5
Dudeldups <130840918+Dudeldups@users.noreply.github.com>
2024-08-03 22:11:42 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,10 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
-export default defineConfig({
-  base: process.env.VITE_BASE_PATH || "/",
-});
+export default ({ mode }) => {
+  process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ""));
+
+  return defineConfig({
+    base: process.env.VITE_BASE_PATH,
+    plugins: [],
+  });
+};
