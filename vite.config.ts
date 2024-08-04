ba8a0e8a6fd032f867e67ce32f6eca4923fc72a3
Dudeldups <130840918+Dudeldups@users.noreply.github.com>
2024-08-04 10:31:25 +0200

    added vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
+/// <reference types="vitest" />
 import { defineConfig, loadEnv } from "vite";
+import { configDefaults } from "vitest/config";
 
 export default ({ mode }) => {
   process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ""));
@@ -6,5 +8,10 @@ export default ({ mode }) => {
   return defineConfig({
     base: process.env.VITE_BASE_PATH,
     plugins: [],
+    test: {
+      globals: true,
+      environment: "jsdom",
+      exclude: [...configDefaults.exclude, "tests/e2e/*"],
+    },
   });
 };
