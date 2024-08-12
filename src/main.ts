84edfd189c69fb61ded1761e960247f131b01fa4
Dudeldups <130840918+Dudeldups@users.noreply.github.com>
2024-08-12 17:56:12 +0200

    added validation function, css changes

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
 import "./index.css";
+import { validateEmailInput } from "./utils/validateEmailInput";
+
+const formEl = document.querySelector("form") as HTMLFormElement;
+
+formEl.addEventListener("submit", event => {
+  event.preventDefault();
+  validateEmailInput();
+});
--- src/utils/validateEmailInput.ts
+++ src/utils/validateEmailInput.ts
@@ -1,19 +1,31 @@
 import { isValidEmail } from "./isValidEmail";
 
 const validateEmailInput = () => {
-  const emailEl = document.querySelector("#email") as HTMLInputElement;
-  const email = emailEl.value.trim();
+  const emailInputEl = document.querySelector("#email") as HTMLInputElement;
+  const email = emailInputEl.value.trim();
 
   const emailErrorEl = document.querySelector(
     "#email-error"
   ) as HTMLParagraphElement;
 
-  if (email === "") {
-    emailErrorEl.textContent = "Email is required";
-  } else if (!isValidEmail(email)) {
-    emailErrorEl.textContent = "Please enter a valid email address";
+  if (isValidEmail(email)) {
+    emailErrorEl.textContent = "Thanks for signing up!";
+    emailInputEl.classList.remove("border-error");
+    emailInputEl.classList.add("border-cyan");
+    emailInputEl.setAttribute("aria-invalid", "false");
+    emailErrorEl.classList.remove("text-error");
+    emailErrorEl.classList.add("text-cyan");
   } else {
-    emailErrorEl.textContent = "";
+    if (email === "") {
+      emailErrorEl.textContent = "Email is required";
+    } else {
+      emailErrorEl.textContent = "Please enter a valid email address";
+    }
+    emailInputEl.classList.remove("border-cyan");
+    emailInputEl.classList.add("border-error");
+    emailInputEl.setAttribute("aria-invalid", "true");
+    emailErrorEl.classList.remove("text-cyan");
+    emailErrorEl.classList.add("text-error");
   }
 };
 
